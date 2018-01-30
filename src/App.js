ffa8134348bc6909f23a2295b9fdbb74804bd7af
Noeliiap17 <n.parra2003@gmail.com>
2018-01-30 12:32:50 +0100

    paint name img and house

--- src/App.js
+++ src/App.js
@@ -1,18 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+//import Charecters from './Charecters';
 
 class App extends Component {
+
+	constructor(props){
+    super(props);
+    // this. = this..bind(this);
+    this.state = {
+      characters: [],
+    };
+  }
+
+	componentDidMount() {
+		fetch ('http://hp-api.herokuapp.com/api/characters')
+		.then(request => request.json())
+		.then(json => {
+			this.setState({
+				characters: json
+			});
+		//	console.log(this.state.characters);
+		});
+
+	}
+
+	paintCharacters() {
+		const listName = [];
+
+	for(const listCharacters of this.state.characters){
+
+		listName.push(<li className="listPerson"><div class="Container__person">
+			<h2>{listCharacters.name}</h2>
+			<img src={listCharacters.image} className="image"/>
+			<span>{listCharacters.house}</span>
+			<span>{listCharacters.alive? 'vivo': 'muerto'}</span>
+
+			</div></li>);
+	}
+	return listName;
+}
+
+filterCharacters() {
+	
+}
+
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">My Harry Potter Characters</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+				<main>
+				<div className="Container__page">
+				<input type="text" className="Seeker" name="Seeker" />
+					<div className="Container__characters">
+					<ul>{this.paintCharacters()}</ul>
+					</div>
+				</div>
+
+				</main>
       </div>
     );
   }
---
+++ src/components/Characters.js
@@ -0,0 +1,17 @@
+// import React from '.react';
+//
+// class Characters extends React.Component {
+// render(){
+// 	return(
+// 		<div>
+// 		<h2>this.props.name</h2>
+// 		</div>
+// 	);
+// }
+// }
+//
+//
+//
+//
+//
+// export default Characters;
